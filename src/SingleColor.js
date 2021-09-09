1641db675b8af3f0c6151e7ad1b29c97abac3fb6
Alex <alexbachurin91@gmail.com>
2021-09-09 19:29:21 +0300

    show alert that coppied succesfully, then remove it after given time

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
-export default function SingleColor({ rgb, weight, index, type }) {
+export default function SingleColor({ rgb, weight, type }) {
+    //alert to inform user that color is succesfully copied to clipboard
+    const [alert, setAlert] = useState(false);
     //transform rgb to hex value
     const hex = rgbToHex(...rgb);
     let colorCls = "color";
     if (type === 'shade') {
         colorCls += ' color-light'
     }
+
+    // copy handler function
+    const copyToClipboard = () => {
+        setAlert(true);
+    }
+    //change alert to false in 3 seconds and clear it
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setAlert(false)
+        }, 3000)
+
+        return () => {
+            clearInterval(timerId);
+        }
+    }, [alert])
     return (
         // dynamically add style to item
-        <article className={colorCls} style={{ backgroundColor: hex }}>
+        <article onClick={copyToClipboard} className={colorCls} style={{ backgroundColor: hex }}>
             <p className="percent-value">{weight}%</p>
             <p className="color-value">{hex}</p>
+            {alert ? <p className='alert'>copied to clipboard</p> : null}
         </article>
     )
 }
