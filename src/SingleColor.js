ff0d42b4a1162fb2d685d27fd493cb67a36db904
Alex <alexbachurin91@gmail.com>
2021-09-09 19:12:18 +0300

    dynamically change colors based on type of color: tint or shade

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import rgbToHex from './utils'
-export default function SingleColor({ rgb, weight, index }) {
+export default function SingleColor({ rgb, weight, index, type }) {
     //transform rgb to hex value
     const hex = rgbToHex(...rgb);
+    let colorCls = "color";
+    if (type === 'shade') {
+        colorCls += ' color-light'
+    }
     return (
         // dynamically add style to item
-        <article className="color" style={{ backgroundColor: hex }}>
+        <article className={colorCls} style={{ backgroundColor: hex }}>
             <p className="percent-value">{weight}%</p>
             <p className="color-value">{hex}</p>
         </article>
